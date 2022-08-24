f7f32127f3c1cf10117290a2cb20b61aaf46c3eb
Mehdi <rhouzm@gmail.com>
2022-08-24 12:10:12 +0200

    new pair

--- contracts/AggregatorV3Interface.sol
+++ contracts/AggregatorV3Interface.sol
@@ -8,28 +8,31 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConsumerV3 {
 
-    AggregatorV3Interface internal priceFeed;
+    AggregatorV3Interface internal priceFeedSAND;
+    AggregatorV3Interface internal priceFeedUSDT;
 
     /**
-     * Network: Goerli
-     * Aggregator: LINK/USD
-     * Address: 0x48731cF7e84dc94C5f84577882c14Be11a5B7456
+     * Network: Mumbai
+     * Aggregator: 2 Paris 
+     * Address: 
      */
     constructor() {
-        priceFeed = AggregatorV3Interface(0x48731cF7e84dc94C5f84577882c14Be11a5B7456);
+        priceFeedSAND = AggregatorV3Interface(0x9dd18534b8f456557d11B9DDB14dA89b2e52e308); //SAND/USD
+        priceFeedUSDT = AggregatorV3Interface(0x92C09849638959196E976289418e5973CC96d645); // USDT/USD
     }
+
 
     /**
      * Returns the latest price
      */
-    function getLatestPrice() public view returns (int) {
+    function getLatestPrice(AggregatorV3Interface _priceFeed) public view returns (int) {
         (
             /*uint80 roundID*/,
             int price,
             /*uint startedAt*/,
             /*uint timeStamp*/,
             /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
+        ) = _priceFeed.latestRoundData();
         return price / 1e8; // 
     }
 }
--- contracts/Botminator.sol
+++ contracts/Botminator.sol
@@ -13,8 +13,9 @@ contract botminatorVault is Ownable, PriceConsumerV3{
     using SafeTransfer for IERC20;
 
     IUniswapV2Router02 dex;  
-    address USDTAddress = 0x509Ee0d083DdF8AC028f2a56731412edD63223B9; //Goerli
+    // address USDTAddress = 0x509Ee0d083DdF8AC028f2a56731412edD63223B9; //Goerli
     address LINKAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB; //Goerli
+    address BTCAddress = ; // Goerli
 
     //0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D GOERLI
     constructor(address _router) {
@@ -33,17 +34,17 @@ contract botminatorVault is Ownable, PriceConsumerV3{
     function Hedger(uint256 amountIn) private {
 
 
-        // USDT --> LINK          -------- UNISWAP --------- 
+        // BTC--> LINK          -------- UNISWAP --------- 
         //Sending USDT to the vault and approving 
-        uint priceFeedUSDT = uint(getLatestPrice());
-        uint PriceOracleEntry = priceFeedUSDT * amountIn;
+        uint priceFeed1 = uint(getLatestPrice(priceFeedBTC));
+        uint PriceOracleEntry = priceFeed1 * amountIn;
 
-        require(IERC20(USDTAddress).transferFrom(msg.sender, address(this), amountIn), 'failed');
-        require(IERC20(USDTAddress).approve(router, amountIn), 'failed');
+        require(IERC20(BTCAddress).transferFrom(msg.sender, address(this), amountIn), 'failed');
+        require(IERC20(BTCAddress).approve(router, amountIn), 'failed');
 
         //Swapping 
 		address[] memory tokens = new address[](2);
-		tokens[0] = USDTAddress;
+		tokens[0] = BTCAddress;
 		tokens[1] = LINKAddress;
 		uint maxTimeToSwap = block.timestamp + 300;
         uint amountOutMin = dex.getAmountsOut(amountIn, tokens)[1]; //AmountOutMin of LINK TOKEN
@@ -52,24 +53,24 @@ contract botminatorVault is Ownable, PriceConsumerV3{
         require(amounts > 0, "Transaction aborted");
 
 
-        //Reverse Swap : LINK --> USDT        ------ QUICKSWAP ------ 
+        //Reverse Swap : LINK --> BTC       ------ QUICKSWAP ------ 
         //Calcul of new input token based on last price of Output Token :
-        uint priceFeed2 = uint(getLatestPrice());
-        uint newAmountIn = amounts - ((amountIn * priceFeedUSDT)/priceFeed2);
+        uint priceFeed2 = uint(getLatestPrice(priceFeedBTC));
+        uint newAmountIn = amounts - ((amountIn * priceFeed1)/priceFeed2);
 
         //DO SECOND SWAP 
         require(IERC20(LINKAddress).transferFrom(msg.sender, address(this), newAmountIn), 'failed');
         require(IERC20(LINKAddress).approve(router, newAmountIn), 'failed'); // Allowance of LINK 
         address[] memory reverseTokens = new address[](2);
         reverseTokens[0] = LINKAddress; 
-        reverseTokens[1] = USDTAddress;
+        reverseTokens[1] = BTCAddress;
         uint amountOutMin2 = dex.getAmountsOut(amounts, reverseTokens)[1]; //AmountOutMin of USDT Token
-        uint USDTExit = dex.swapExactTokensForTokens(newAmountIn, amountOutMin2, reverseTokens, address(this), maxTimeToSwap)[1];
+        uint BTCExit = dex.swapExactTokensForTokens(newAmountIn, amountOutMin2, reverseTokens, address(this), maxTimeToSwap)[1];
 
         // Arbitrage or not ?
         //uint newUSDTBalance = IERC20(USDTAddress).balanceOf(address(this));
-        uint priceFeedLast = uint(getLatestPrice());
-        uint PriceOracleExit = priceFeedLast * USDTExit; 
+        uint priceFeedLast = uint(getLatestPrice(priceFeedBTC));
+        uint PriceOracleExit = priceFeedLast * BTCExit; 
 
         require(PriceOracleEntry <= PriceOracleExit, "Proof of Price Variation not valid ");
 
