88ffa2a42f7ba9edb8d6bda8f2dc61e92fee2926
Mehdi <rhouzm@gmail.com>
2022-08-24 14:01:46 +0200

    chainlink price feed fixed

--- test/Botminator.js
+++ test/Botminator.js
@@ -29,8 +29,30 @@ describe("Botminator", function() {
       const price= await PRICE.deploy();
       await price.deployed();
       console.log('Price deployed to', price.address);
-      // const latestPrice = await price.getLatestPrice();
-      // console.log("Latest price:", latestPrice);
+      const aggregatorV3InterfaceABI = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
+      const SANDUSD = "0x3D49406EDd4D52Fb7FFd25485f32E073b529C924" // SAND/USD
+      const USDTUSD = "0x0A6513e40db6EB1b165753AD52E80663aeA50545" // USDT/USD
+      const provider = new hre.ethers.providers.JsonRpcProvider("https://rpc.ankr.com/polygon")
+      const getPriceSAND = new hre.ethers.Contract(SANDUSD, aggregatorV3InterfaceABI, provider)
+      const getPriceUSDT = new hre.ethers.Contract(USDTUSD, aggregatorV3InterfaceABI, provider)
+      await getPriceSAND.latestRoundData()
+        .then((roundData) => {
+          // Do something with roundData
+          let priceInHex = roundData.answer._hex;
+          const hexToDecimal = hex => parseInt(hex, 16)
+          const price = hexToDecimal(priceInHex)
+          console.log(`latest price of SAND: ${price * 10**-8} USD`);
+        })
+
+      await getPriceUSDT.latestRoundData()
+      .then((roundData) => {
+        // Do something with roundData
+        let priceInHex = roundData.answer._hex;
+        const hexToDecimal = hex => parseInt(hex, 16)
+        const price = hexToDecimal(priceInHex)
+        console.log(`latest price of USDT: ${price * 10**-8} USD`);
+      })
+
     })
   });
 
